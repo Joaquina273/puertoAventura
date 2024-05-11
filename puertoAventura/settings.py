14b873f4b5001366a82739b11e61a5d357e4c06d
nicolasdelgado01 <nicovieii99@gmail.com>
2024-05-11 12:42:47 -0300

    no anda 2

--- puertoAventura/settings.py
+++ puertoAventura/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "puertoAventura.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'inicio/templates')],
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- puertoAventura/urls.py
+++ puertoAventura/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('autenticacion/', include('autenticacion.urls')),
-    path('', include('inicio.urls'))
 ]
