14b9b4459a6ad255b6dcf0fdb77070d8d8d0d068
nicolasdelgado01 <nicovieii99@gmail.com>
2024-05-11 12:06:28 -0300

    agregando inicio

--- puertoAventura/settings.py
+++ puertoAventura/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -57,7 +57,7 @@ ROOT_URLCONF = "puertoAventura.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'inicio/templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
