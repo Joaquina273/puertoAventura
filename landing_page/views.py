423ae9c347822c794cbf3d9a17430f9484454db5
nicolasdelgado01 <nicovieii99@gmail.com>
2024-05-19 19:16:11 -0300

    agregando rol

--- landing_page/views.py
+++ landing_page/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from db.models import Post
+from db.models import Post,User
 
 # Create your views here.
 
 def landing_page(request):
-
-    return render(request, "home.html",{'usuario': request.session.get('usuario')})
+    usuario=request.session.get('usuario')
+    if usuario:
+        user = User.objects.get(email=usuario[0])
+    else: 
+        user = usuario
+    return render(request, "home.html",{'usuario': user})
 
 
 def visualizar_publicaciones_finalizadas(request):
