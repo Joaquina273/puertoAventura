5c5ca575e3ab7dee70089092cf830b00a9fb2dc3
León Felder <leonfelder45@gmail.com>
2024-06-09 18:22:09 -0300

    acortador de notificaciones

--- db/context_processors.py
+++ db/context_processors.py
@@ -4,6 +4,9 @@ from db.models import User
 def navbar_variables (request):
     if (request.session.get('usuario') != None):
         notificaciones = Notification.objects.order_by("-date").filter(user=request.session.get('usuario')[0])[0:3]
+        for noti in notificaciones:
+            if (len(noti.content) > 75):
+                noti.content = f'{" ".join(noti.content.split()[0:9])}...'
         sin_leer = Notification.objects.filter(user=request.session.get('usuario')[0]).filter(read=False)
         user=User.objects.filter(email=request.session.get('usuario')[0]).first
           
