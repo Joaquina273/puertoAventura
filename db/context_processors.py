93960d81c53d1b66284131478e672896967171f0
León Felder <leonfelder45@gmail.com>
2024-07-05 09:56:05 -0300

    Chats notificables baby

--- db/context_processors.py
+++ db/context_processors.py
@@ -1,5 +1,5 @@
-from db.models import Notification, Conversation
-from db.models import User
+from db.models import Notification, Conversation, User, Message
+from django.db.models import Q
 
 def navbar_variables (request):
     if (request.session.get('usuario') != None):
@@ -10,15 +10,27 @@ def navbar_variables (request):
         sin_leer = Notification.objects.filter(user=request.session.get('usuario')[0]).filter(read=False)
         user=User.objects.filter(email=request.session.get('usuario')[0]).first
         chats = None
-        #chats = Conversation.objects.order_by("-updated_at").filter(sender=request.session.get('usuario')[0] or recipient=request.session.get('usuario')[0])
+        chats = Conversation.objects.order_by("-updated_at").filter(Q(sender=request.session.get('usuario')[0]) | Q(recipient=request.session.get('usuario')[0]))
+        mensajes_sin_leer = 0
+        chats = list(chats)
+        for chat in range(len(chats)):
+            no_leido = Message.objects.filter(conversation=chats[chat]).exclude(sender=request.session.get('usuario')[0]).exclude(read=True).exists()
+            if (no_leido):
+                mensajes_sin_leer = mensajes_sin_leer + 1
+                chats[chat] = {'content': chats[chat], 'read' : False}
+            else:
+                chats[chat] = {'content': chats[chat], 'read' : True}
+            
     else:
         notificaciones = None
         sin_leer = None
         user = None
         chats = None
+        mensajes_sin_leer = None
     return {
         'usuario' : user,
         'notificaciones' : notificaciones,
         'sin_leer' : sin_leer,
-        'chats' : chats
+        'chats' : chats,
+        'mensajes_sin_leer' : mensajes_sin_leer
     }
